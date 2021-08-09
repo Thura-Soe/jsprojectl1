488d84627e1405fe41adb2b944d10eec3642b6f0
Thura-Soe <thurasoe797979@gmail.com>
2021-08-09 18:26:08 +0630

    update 8-9

--- audioplayer/js/app.js
+++ audioplayer/js/app.js
@@ -49,6 +49,7 @@ function playsong(){
     musicconatiner.classList.add("play");
     playbtn.querySelector("i.fas").classList.remove("fa-play");
     playbtn.querySelector("i.fas").classList.add("fa-pause");
+
     audio.play();
 
     // console.log(audio.duration);
@@ -59,7 +60,96 @@ function pausesong(){
     musicconatiner.classList.remove("play")
     playbtn.querySelector("i.fas").classList.remove("fa-pause");
     playbtn.querySelector("i.fas").classList.add("fa-play");
+
     audio.pause();
 
 }
 
+// Change Song 
+prevbtn.addEventListener("click", prvioussong);
+nextbtn.addEventListener("click", nextsong);
+
+
+// Previous Song 
+function prvioussong(){
+    // console.log('hay');
+    songindex--;
+    // songindex -= 1;
+
+    if(songindex < 0){
+        songindex = songs.length - 1;
+    }
+
+    // console.log(songindex);
+    loadsong(songs[songindex]);
+    playsong();
+}
+
+// Next Song 
+function nextsong(){
+    // console.log('hay');
+
+    // songindex++;
+    songindex += 1;
+    if(songindex > songs.length -1 ){
+        songindex = 0;
+    }
+    // console.log(songindex);
+
+    loadsong(songs[songindex]);
+    playsong();
+}
+
+// Time Play and Stop Update 
+audio.addEventListener('timeupdate', updateprogress);
+
+
+// Update Progress Bar 
+function updateprogress(e){
+
+    // console.log(audio.currentTime);
+    // console.log(audio.durationp);
+
+    // Method 1
+    // const progresspercent = (audio.currentTime / audio.duration) * 100;
+    // console.log(progresspercent);
+    // progress.style.width = `${progresspercent}%`;
+
+    // Method 2
+    // const currenttime = e.target.currentTime;
+    // const duration = e.target.duration;
+    // const progresspercent = (currenttime / duration) * 100;
+    // progress.style.width = `${progresspercent}%`;
+
+    // Method 3
+    // const {currentTime} = e.target;
+    // const {duration} = e.target;
+    // const progresspercent = (currentTime / duration) * 100;
+    // progress.style.width = `${progresspercent}%`;
+
+    // Method 4
+    const {currentTime, duration} = e.target;
+    const progresspercent = (currentTime / duration) * 100;
+    progress.style.width = `${progresspercent}%`;
+
+}
+
+// Click on Progress Bar 
+progresscontainer.addEventListener('click', setprogress);
+
+// Set Progress Bar 
+function setprogress(e){
+    // console.log(e.target);
+
+    const width = e.target.clientWidth;
+    // console.log(width);
+
+    const clickx = e.offsetX;
+    // console.log(clickx);
+    const duration = audio.duration;
+
+    audio.currentTime = (clickx / width) * duration;
+}
+
+// Song End 
+audio.addEventListener('ended', nextsong);
